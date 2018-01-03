1af82774c39f4751473fdb20cfb42a95efeef455
Saroj Adhikari <sarojadhikarix@gmail.com>
2018-01-03 16:05:48 +0100

    changed auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
 
-        Passport::routes();
+        //Passport::routes();
         Passport::routes(null, ['middleware' => [ \Barryvdh\Cors\HandleCors::class ]]);
         //Passport::tokensExpireIn(Carbon::now()->addDays(2));
 
